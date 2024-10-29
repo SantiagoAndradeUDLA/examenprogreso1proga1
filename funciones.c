af5f1482e07147c313a745ea9b2742e1a107edc8
SantiagoAndradeUDLA <santiago.adndrade.bucheli@udla.edu.ec>
2024-10-29 10:18:57 -0500

    :)

---
+++ funciones.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.c"
+
+void (ingresar[MAX][30]; edad[MAX][]; estado[MAX][] ){
+ke = 1
+status = bool
+
+ 
+//PARA RECIBIR ESTADO:
+
+
+scanf 
+if ()
+{
+    /* code */
+}
+
+
+ persona++;
+
+    return 0;
+}
+
+
+void (buscaredad[MAX][]){
+    return 0;
+}
+
+
+void (buscarestado[MAX][]){
+
+
+
+    return 0;
+}
+
+    printf = "seleccione un numero, 1. soltero 2.casado\n"
---
+++ mian.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <string.h>
+
+MAX = 5;
+ffswitch = 0;
+persona = 0;
+
+
+printf = "Seleccione una opcion\n";
+printf = "1.anadir persona 2.buscar por estado estado 3. buscar por edad 4. \n";
+
+
+while (ffswitch =! 4)
+{
+
+scanf ffswitch;
+
+    switch (expression)
+{
+case ffswitch = 1:
+    
+        if (persona < MAX)
+        {
+            /* EJECUTARLEEREPRSONA */
+        }
+        else{
+
+            printf"ya se ha llegado al maximo de personas  \n";
+        }
+
+    break;
+case ffswitch = 2:
+
+
+    /* code */
+
+    
+    break;
+case ffswitch = 3:
+    /* code */
+    break; 
+
+default:
+    break; 
+}
+
+
+
+}
+
+return 0;
+
+
